05d5b089755d9d91cc82c196a784d3824e458fcb
RemovedQuasar <kuno98200@yahoo.it>
2016-03-09 15:34:42 +0100

    Abbiamo aggiornato l'entity bean User

--- MobaTripAd/src/moba/model/entity/Utente.java
+++ MobaTripAd/src/moba/model/entity/Utente.java
@@ -1,7 +1,75 @@
 package moba.model.entity;
 
+
+import java.sql.Timestamp;
+
 public class Utente {
 	
-	//bean x tabella DB UTENTI
+	private Integer idUtente;
+	private Boolean admin;
+	private String nickname;
+	private String email;
+	private String nome;
+	private String cognome;
+	private Grado grado;
+	private String avatar;
+	private Timestamp dataReg;
+	private String info;
+	
+	public Utente(Integer idUtente, Boolean admin, String nickname, String email, String nome, String cognome,
+			Grado grado, String avatar, Timestamp dataReg, String info) {
+		super();
+		
+		this.idUtente = idUtente;
+		this.admin = admin;
+		this.nickname = nickname;
+		this.email = email;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.grado = grado;
+		this.avatar = avatar;
+		this.dataReg = dataReg;
+		this.info = info;
+	}
+	public Integer getIdUtente() {
+		return idUtente;
+	}
+	public Boolean getAdmin() {
+		return admin;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public Grado getGrado() {
+		return grado;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public Timestamp getDataReg() {
+		return dataReg;
+	}
+	public String getInfo() {
+		return info;
+	}
+	@Override
+	public String toString() {
+		return 
+				
+				" /n Utente [idUtente=" + idUtente + ", admin=" + admin + ", nickname=" + nickname + ", email=" + email
+				+ ", nome=" + nome + ", cognome=" + cognome + ", grado=" + grado + ", avatar=" + avatar + ", dataReg="
+				+ dataReg + ", info=" + info + "]";
+	}
+	
+	
 
 }
