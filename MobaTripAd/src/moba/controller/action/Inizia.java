900f625fb24e478fbf383c581baba4e96dc9a2a4
ftrombetta91@gmail.com <ftrombetta91@gmail.com>
2016-03-11 11:54:15 +0100

    inizia.java

--- MobaTripAd/src/moba/controller/action/Inizia.java
+++ MobaTripAd/src/moba/controller/action/Inizia.java
@@ -9,7 +9,9 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import moba.model.dao.DAO;
+import moba.model.dao.DaoCategoria;
 import moba.model.dao.DaoGioco;
+import moba.model.dao.DaoPiattaforma;
 import moba.model.dao.eccezioni.DAOException;
 import moba.model.dao.enumeratori.Tabella;
 import moba.model.entity.Gioco;
@@ -22,9 +24,14 @@ public class Inizia extends Action {
 
 		try {
 			DaoGioco dao = (DaoGioco) DAO.getDaoInstance(Tabella.Gioco);
-
-			request.setAttribute("ultimoGiocoUscito",(Gioco) dao.selectLast());
+			DaoPiattaforma daop = (DaoPiattaforma) DAO.getDaoInstance(Tabella.Piattaforma);
+			DaoCategoria daoc = (DaoCategoria) DAO.getDaoInstance(Tabella.Categoria);
+			
+			request.setAttribute("ultimoGiocoUscito", dao.selectLast());
 			request.setAttribute("giochiPiuRecensito", dao.selectPopolare());
+			request.setAttribute("listaPiattaforme", daop.select());
+			request.setAttribute("listaCategorie", daoc.select());
+			
 			return mapping.findForward("success");
 		} catch (DAOException e) {
 
