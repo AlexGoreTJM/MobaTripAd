ae070fb66d5e1c771c7961d585d871e3d73289e9
chrisource <c.rascioni@gmail.com>
2016-03-11 15:07:48 +0100

    aggiunti path in sessione

--- MobaTripAd/src/moba/controller/action/Inizia.java
+++ MobaTripAd/src/moba/controller/action/Inizia.java
@@ -2,6 +2,7 @@ package moba.controller.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
@@ -25,11 +26,16 @@ public class Inizia extends Action {
 			DaoGioco dao = (DaoGioco) DAO.getDaoInstance(Tabella.Gioco);
 			DaoPiattaforma daop = (DaoPiattaforma) DAO.getDaoInstance(Tabella.Piattaforma);
 			DaoCategoria daoc = (DaoCategoria) DAO.getDaoInstance(Tabella.Categoria);
+			HttpSession session = request.getSession();
 			
 			request.setAttribute("giocoRecente", dao.selectRecente());
 			request.setAttribute("listaGiocoPopolari", dao.selectPopolare());
 			request.setAttribute("listaPiattaforma", daop.select());
 			request.setAttribute("listaCategoria", daoc.select());
+			
+			session.setAttribute("pathCategoria", "/IMGDB/Categoria/");
+			session.setAttribute("pathPiattaforma", "/IMGDB/Piattaforma/");
+			session.setAttribute("pathGioco", "/IMGDB/Gioco/");
 			
 			return mapping.findForward("success");
 		} catch (DAOException e) {
