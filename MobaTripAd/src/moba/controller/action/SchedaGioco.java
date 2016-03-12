e00b6550bb445d915e47aaf175528896e5636e5d
skyzzo <matteo.digirolamo@hotmail.it>
2016-03-12 09:23:26 +0100

    schedaGioco

--- MobaTripAd/src/moba/controller/action/SchedaGioco.java
+++ MobaTripAd/src/moba/controller/action/SchedaGioco.java
@@ -1,5 +1,37 @@
 package moba.controller.action;
 
-public class SchedaGioco {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import moba.model.dao.DAO;
+import moba.model.dao.DaoGioco;
+import moba.model.dao.eccezioni.DAOException;
+import moba.model.dao.enumeratori.Tabella;
+
+public class SchedaGioco extends Action{
+
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		int idGioco = Integer.parseInt(request.getParameter("idGioco"));
+		try {
+			DaoGioco daoGioco = (DaoGioco) DAO.getDaoInstance(Tabella.Gioco);
+
+			request.setAttribute("gioco", daoGioco.select(idGioco));
+
+			return mapping.findForward("success");
+
+		} catch (DAOException e) {
+			request.setAttribute("errore", e.getMessage());
+			return mapping.findForward("failure");
+		}
+
+	}
+	
 }
