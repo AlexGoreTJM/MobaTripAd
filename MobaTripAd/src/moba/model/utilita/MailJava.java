8f01a6ed804b90168862d7e1d8ddca6836e92994
RemovedQuasar <kuno98200@yahoo.it>
2016-03-15 12:36:35 +0100

    Gioco PDF

--- MobaTripAd/src/moba/model/utilita/MailJava.java
+++ MobaTripAd/src/moba/model/utilita/MailJava.java
@@ -3,13 +3,19 @@ package moba.model.utilita;
 import java.util.Properties;
 import java.util.UUID;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.http.HttpSession;
 
 import org.eclipse.jdt.internal.compiler.ast.ThisReference;
@@ -51,6 +57,58 @@ public class MailJava {
  		} catch (MessagingException e) {
  			throw new RuntimeException(e);
  		}
+    	 
+     }
+     
+     public static void MandaGiocoPDFMail(String email, String path, String titolo){
+    	Properties props = new Properties();
+  		props.put("mail.smtp.host", "smtp.libero.it");
+  		props.put("mail.smtp.socketFactory.port", "465");
+  		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+  		props.put("mail.smtp.auth", "true");
+  		props.put("mail.smtp.port", "465");
+  		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+  			@Override
+  			protected PasswordAuthentication getPasswordAuthentication() {
+  				return new PasswordAuthentication("mobatripadproject@libero.it", "Password2");
+  			}
+  		});
+
+  		try {
+
+  			Message message = new MimeMessage(session);
+  			message.setFrom(new InternetAddress("mobatripadproject@libero.it"));
+  			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+  			message.setSubject("PDF Gioco");
+  			
+  			
+  			message.setText("Ecco il pdf del gioco.");
+  			
+  			
+  			
+  			MimeBodyPart messageBodyPart = new MimeBodyPart();
+  	        Multipart multipart = new MimeMultipart();
+  	        messageBodyPart = new MimeBodyPart();
+  	        String file = path;
+  	        String fileName = titolo;
+  	        DataSource source = new FileDataSource(file);
+  	        messageBodyPart.setDataHandler(new DataHandler(source));
+  	        messageBodyPart.setFileName(fileName);
+  	        multipart.addBodyPart(messageBodyPart);
+
+  	        message.setContent(multipart);
+  			
+
+  			Transport.send(message);
+
+  			System.out.println("Done");
+
+  		} catch (MessagingException e) {
+  			throw new RuntimeException(e);
+  		}
+    	 
+    	 
+    	 
     	 
      }
 
