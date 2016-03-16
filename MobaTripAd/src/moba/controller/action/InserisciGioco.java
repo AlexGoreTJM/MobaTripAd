c0ca8fd9c2c9cc0d85bc99342845646fc2ca95a7
chrisource <c.rascioni@gmail.com>
2016-03-16 12:04:45 +0100

    visualizzazione img

--- MobaTripAd/src/moba/controller/action/InserisciGioco.java
+++ MobaTripAd/src/moba/controller/action/InserisciGioco.java
@@ -32,6 +32,7 @@ public class InserisciGioco extends Action {
 
 		// Get the servers upload directory real path name
 		String filePath = "C:\\Users\\ats\\git\\MobaTripAd\\MobaTripAd\\WebContent\\IMGDB\\Gioco";
+//		String tomcat = "C:\\Program Files\\apache-tomcat-8.0.32\\webapps\\MobaTripAd\\IMGDB\\Gioco";
 
 		// create the upload folder if not exists
 		File folder = new File(filePath);
